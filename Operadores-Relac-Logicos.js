fad60434038c5dbd292e9f859e8e0da14d87ffef
Amyr Kamel e Silva <67295939+amyrks1992@users.noreply.github.com>
2021-01-12 20:13:34 -0300

    Add files via upload

---
+++ Operadores-Relac-Logicos.js
@@ -0,0 +1,26 @@
+var nome = 'João'
+var nome2 = 'Maria'
+var idade = 20
+var idade2 = 15
+
+//Operadores Relacionais//
+
+console.log (idade > idade2) 
+console.log (idade < idade2)
+console.log (idade >= idade2)
+console.log (idade <= idade2)
+
+console.log (nome == nome2) //Igual//
+console.log (nome != nome2) //Diferente//
+
+//Operadores Lógicos//
+
+console.log(idade == 20 && idade2 == 15) //&& Conjunção//
+
+console.log(idade != 20 && idade2 == 15)
+
+console.log(nome == 'João' || nome2 != 'Maria') // || Disjunção //
+
+console.log(nome != 'João' || nome2 == 'Marcos')
+
+console.log(!nome == 'João') // ! Negação //
